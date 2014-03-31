1704691d6d9d56408bd5e6dacbb9cf8b7a358033
X8462 <cbijayananda@luc.edu>
2014-03-30 21:11:45 -0500

    initial

---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\cbijayananda.TOYODA\Desktop\EZCardsII\EZCards\libraries\tess-two\bin\AndroidManifest.xml : \
+C:\Users\cbijayananda.TOYODA\Desktop\EZCardsII\EZCards\libraries\tess-two\AndroidManifest.xml \
