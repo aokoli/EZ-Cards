360be9198eddd8fc3b243e1b1ae7b0f2a8f507c6
X8462 <cbijayananda@luc.edu>
2014-04-17 14:20:00 -0500

    Email and SMS functions from the App

--- EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/ProfileActivity.java
+++ EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/ProfileActivity.java
@@ -12,18 +12,22 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ProfileActivity extends Activity{
 
-    private Button button;
+    private Button btnCall;
+    private Button btnMsg;
+    private Button btmEmail;
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        button = (Button) findViewById(R.id.buttonCall);
+        //Phone Call:
+        btnCall = (Button) findViewById(R.id.buttonCall);
 
         //PhoneStateListener
         PhoneCallListener phoneListener = new PhoneCallListener();
@@ -32,8 +36,8 @@ public class ProfileActivity extends Activity{
         telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
 
 
-        //button listener
-        button.setOnClickListener(new OnClickListener() {
+        //btnCall listener
+        btnCall.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
@@ -45,6 +49,54 @@ public class ProfileActivity extends Activity{
             }
 
         });
+
+        //SMS Message:
+        btnMsg = (Button) findViewById(R.id.buttonSend);
+
+        btnMsg.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                    sendIntent.setData(Uri.parse("sms:2517764076"));
+                    sendIntent.putExtra("sms_body", "default content");
+                    startActivity(sendIntent);
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Email:
+        btmEmail = (Button) findViewById(R.id.buttonSendEmail);
+        btmEmail.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String to = "cbijayananda@luc.edu";
+                String subject = "EZCards Email";
+                String message = "Niiiiice!!";
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //need this to prompts email client only
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+            }
+        });
+
 
     }
 
