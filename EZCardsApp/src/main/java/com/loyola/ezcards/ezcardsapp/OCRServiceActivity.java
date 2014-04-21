ae5a60fe654b90fd4828c5db9880f0ee0c1e01ed
X8462 <cbijayananda@luc.edu>
2014-04-21 18:00:40 -0500

    Use of Intent replacing SetContentView().. Reviewed by @Shikha

--- EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/OCRServiceActivity.java
+++ EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/OCRServiceActivity.java
@@ -1,6 +1,7 @@
 package com.loyola.ezcards.ezcardsapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -18,6 +19,7 @@ import android.widget.ImageView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -45,12 +47,12 @@ public class OCRServiceActivity extends Activity {
 
     private static final String TAG = "SimpleAndroidOCR.java";
 
-    protected Button _button;
+//    protected Button _button;
     protected ImageView _image;
-    protected EditText _field;
-    protected EditText nameField;
-    protected EditText phoneField;
-    protected EditText emailField;
+//    protected EditText _field;
+//    protected EditText nameField;
+//    protected EditText phoneField;
+//    protected EditText emailField;
     protected String _path;
     protected boolean _taken;
 
@@ -203,7 +205,6 @@ public class OCRServiceActivity extends Activity {
             Log.e(TAG, "Couldn't correct orientation: " + e.toString());
         }
 
-        // _image.setImageBitmap( bitmap );
 
         Log.v(TAG, "Before baseApi");
 
@@ -215,92 +216,14 @@ public class OCRServiceActivity extends Activity {
         String recognizedText = baseApi.getUTF8Text();
 
         baseApi.end();
-        setContentView(R.layout.cardcapture);
-        _image = (ImageView) findViewById(R.id.imageView);
-        nameField = (EditText) findViewById(R.id.FirstName);
-        phoneField = (EditText) findViewById(R.id.PhoneMain);
-        emailField = (EditText) findViewById(R.id.email);
-        _field = (EditText) findViewById(R.id.addressT);
-        _image.setImageBitmap(bitmap);
 
-        // You now have the text in recognizedText var, you can do anything with it.
-        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-        // so that garbage doesn't make it to the display.
+        Intent intent = new Intent(this, SelectCardsActivity.class);
+        intent.putExtra("RecognizedText", recognizedText);
+        intent.putExtra("OCRImagePath", _path);
 
-        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+        startActivity(intent);
 
-        /*if ( lang.equalsIgnoreCase("eng") ) {
-            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
-        }*/
-
-        String allText = "";
-
-        allText = allText + "\n" + recognizedText;
-        //recognizedText = recognizedText.trim();
-
-        if ( recognizedText.length() != 0 ) {
-            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
-            _field.setSelection(_field.getText().toString().length());
-        }
-
-        Pattern p;
-        Matcher m;
-
-        String DisplayName = null;
-        String phone = null;
-        String emailID = null;
-
-	    /*
-	     * Name-matching Expression - Matches: T.V. Raman Alan Viverette Charles L.
-	     * Chen Julie Lythcott-Haimes - Does not match: Google Google User
-	     * Experience Team 650-720-5555 cell
-	     */
-        p = Pattern.compile("^([A-Z]([a-z]*|\\.) *){1,2}([A-Z][a-z]+-?)+$", Pattern.MULTILINE);
-        m = p.matcher(allText);
-
-        if (m.find()) {
-            DisplayName = m.group().toString();
-        }
-
-	    /*
-	     * Email-matching Expression - Matches: email: raman@google.com
-	     * spam@google.co.uk v0nn3gu7@ice9.org name @ host.com - Does not match:
-	     * #@/.cJX Google c@t
-	     */
-        //p = Pattern.compile("([A-Za-z0-9]+ *@ *[A-Za-z0-9]+(\\.[A-Za-z]{2,4})+)$", Pattern.MULTILINE);
-        //p = Pattern.compile("(.+ *@ *.+(\\..{2,4})+)$", Pattern.MULTILINE);
-        p = Pattern.compile("([^ \n]+ *@ *.+(\\..{2,4})+)$", Pattern.MULTILINE);
-        m = p.matcher(allText);
-
-        if (m.find()) {
-            emailID = m.group(1);
-        }
-
-	    /*
-	     * Phone-matching Expression - Matches: 1234567890 (650) 720-5678
-	     * 650-720-5678 650.720.5678 - Does not match: 12345 12345678901 720-5678
-	     */
-        p = Pattern.compile("(?:^|\\D)(\\d{3})[)\\-. ]*?(\\d{3})[\\-. ]*?(\\d{4})(?:$|\\D)");
-        m = p.matcher(recognizedText);
-
-        if (m.find()) {
-            phone = "(" + m.group(1) + ") " + m.group(2) + "-" + m.group(3);
-        }
-
-
-        //displays results for testing
-        String output = new String();
-        output = "Name: " + DisplayName + "\n" + "Phone: " + phone + "\n" + "Email: " + emailID + "\n";
-
-        nameField.setText(DisplayName);
-        phoneField.setText(phone);
-        emailField.setText(emailID);
-        Log.d(TAG, "Input: " + allText);
-        Log.d(TAG, output);
-
-        // Cycle done.
     }
-
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
--- EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/SelectCardsActivity.java
+++ EZCardsApp/src/main/java/com/loyola/ezcards/ezcardsapp/SelectCardsActivity.java
@@ -2,12 +2,17 @@ package com.loyola.ezcards.ezcardsapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SelectCardsActivity extends Activity {
 
@@ -16,9 +21,35 @@ public class SelectCardsActivity extends Activity {
     private Button btnSaveContact;
     private final static String tag = "SelectCardsActivity";
 
+    protected ImageView _image;
+    protected EditText _field;
+    protected EditText nameField;
+    protected EditText phoneField;
+    protected EditText emailField;
+
+    private String recognizedText = null;
+    private String imagePath = null;
+    private Bitmap bitmap;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cardcapture);
+
+
+        Intent intent = getIntent();
+        recognizedText = intent.getStringExtra("RecognizedText");
+        imagePath = intent.getStringExtra("OCRImagePath");
+
+        Log.v(tag, " imagePath : " + imagePath);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        bitmap = BitmapFactory.decodeFile(imagePath, options);
+
+
+        if(recognizedText != null){
+            fieldMap();
+        }
 
         btnSaveContact = (Button) findViewById(R.id.btnSave);
 
@@ -30,6 +61,93 @@ public class SelectCardsActivity extends Activity {
                         onSaveContact(view);    //view is
                     }
                 });
+    }
+
+    public void fieldMap(){
+
+        _image = (ImageView) findViewById(R.id.imageView);
+        nameField = (EditText) findViewById(R.id.FirstName);
+        phoneField = (EditText) findViewById(R.id.PhoneMain);
+        emailField = (EditText) findViewById(R.id.email);
+        _field = (EditText) findViewById(R.id.addressT);
+        _image.setImageBitmap(bitmap);
+
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+
+        Log.v(tag, "OCRED TEXT: " + recognizedText);
+
+        /*if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }*/
+
+        String allText = "";
+
+        allText = allText + "\n" + recognizedText;
+        //recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            _field.setSelection(_field.getText().toString().length());
+        }
+
+        Pattern p;
+        Matcher m;
+
+        String DisplayName = null;
+        String phone = null;
+        String emailID = null;
+
+	    /*
+	     * Name-matching Expression - Matches: T.V. Raman Alan Viverette Charles L.
+	     * Chen Julie Lythcott-Haimes - Does not match: Google Google User
+	     * Experience Team 650-720-5555 cell
+	     */
+        p = Pattern.compile("^([A-Z]([a-z]*|\\.) *){1,2}([A-Z][a-z]+-?)+$", Pattern.MULTILINE);
+        m = p.matcher(allText);
+
+        if (m.find()) {
+            DisplayName = m.group().toString();
+        }
+
+	    /*
+	     * Email-matching Expression - Matches: email: raman@google.com
+	     * spam@google.co.uk v0nn3gu7@ice9.org name @ host.com - Does not match:
+	     * #@/.cJX Google c@t
+	     */
+        //p = Pattern.compile("([A-Za-z0-9]+ *@ *[A-Za-z0-9]+(\\.[A-Za-z]{2,4})+)$", Pattern.MULTILINE);
+        //p = Pattern.compile("(.+ *@ *.+(\\..{2,4})+)$", Pattern.MULTILINE);
+        p = Pattern.compile("([^ \n]+ *@ *.+(\\..{2,4})+)$", Pattern.MULTILINE);
+        m = p.matcher(allText);
+
+        if (m.find()) {
+            emailID = m.group(1);
+        }
+
+	    /*
+	     * Phone-matching Expression - Matches: 1234567890 (650) 720-5678
+	     * 650-720-5678 650.720.5678 - Does not match: 12345 12345678901 720-5678
+	     */
+        p = Pattern.compile("(?:^|\\D)(\\d{3})[)\\-. ]*?(\\d{3})[\\-. ]*?(\\d{4})(?:$|\\D)");
+        m = p.matcher(recognizedText);
+
+        if (m.find()) {
+            phone = "(" + m.group(1) + ") " + m.group(2) + "-" + m.group(3);
+        }
+
+
+        //displays results for testing
+        String output = new String();
+        output = "Name: " + DisplayName + "\n" + "Phone: " + phone + "\n" + "Email: " + emailID + "\n";
+
+        nameField.setText(DisplayName);
+        phoneField.setText(phone);
+        emailField.setText(emailID);
+        Log.d(tag, "Input: " + allText);
+        Log.d(tag, output);
+
+        // Cycle done.
     }
 
     public void onSaveContact(View view){
